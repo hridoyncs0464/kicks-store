0ab1c265d4f8768e2d9bbd517dc15c5d342b6e7f
Hridoy <hridoyncs0464@gmail.com>
2026-02-21 18:25:31 +0600

    feat: initialise kicks store project with core structure and configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
+ 
+ 
+ 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+   
+   
+      
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
